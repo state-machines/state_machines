8654948aadf51087f409654e5f2b449129686fd9
Abdelkader Boudih <terminale@gmail.com>
2025-06-19 02:28:30 +0100

    fix: extract internal into modules

---
+++ lib/state_machines/machine/helper_generators.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+module StateMachines
+  class Machine
+    module HelperGenerators
+      protected
+
+      # Adds helper methods for interacting with the state machine, including
+      # for states, events, and transitions
+      def define_helpers
+        define_state_accessor
+        define_state_predicate
+        define_event_helpers
+        define_path_helpers
+        define_action_helpers if define_action_helpers?
+        define_name_helpers
+      end
+
+      # Defines the initial values for state machine attributes.  Static values
+      # are set prior to the original initialize method and dynamic values are
+      # set *after* the initialize method in case it is dependent on it.
+      def define_state_initializer
+        define_helper :instance, <<-END_EVAL, __FILE__, __LINE__ + 1
+            def initialize(*)
+              self.class.state_machines.initialize_states(self) { super }
+            end
+        END_EVAL
+      end
+
+      # Adds reader/writer methods for accessing the state attribute
+      def define_state_accessor
+        attribute = self.attribute
+
+        @helper_modules[:instance].class_eval { attr_reader attribute } unless owner_class_ancestor_has_method?(:instance, attribute)
+        @helper_modules[:instance].class_eval { attr_writer attribute } unless owner_class_ancestor_has_method?(:instance, "#{attribute}=")
+      end
+
+      # Adds predicate method to the owner class for determining the name of the
+      # current state
+      def define_state_predicate
+        call_super = !owner_class_ancestor_has_method?(:instance, "#{name}?").nil?
+        define_helper :instance, <<-END_EVAL, __FILE__, __LINE__ + 1
+            def #{name}?(*args)
+              args.empty? && (#{call_super} || defined?(super)) ? super : self.class.state_machine(#{name.inspect}).states.matches?(self, *args)
+            end
+        END_EVAL
+      end
+
+      # Adds helper methods for getting information about this state machine's
+      # events
+      def define_event_helpers
+        # Gets the events that are allowed to fire on the current object
+        define_helper(:instance, attribute(:events)) do |machine, object, *args|
+          machine.events.valid_for(object, *args).map(&:name)
+        end
+
+        # Gets the next possible transitions that can be run on the current
+        # object
+        define_helper(:instance, attribute(:transitions)) do |machine, object, *args|
+          machine.events.transitions_for(object, *args)
+        end
+
+        # Fire an arbitrary event for this machine
+        define_helper(:instance, "fire_#{attribute(:event)}") do |machine, object, event, *args|
+          machine.events.fetch(event).fire(object, *args)
+        end
+
+        # Add helpers for tracking the event / transition to invoke when the
+        # action is called
+        return unless action
+
+        event_attribute = attribute(:event)
+        define_helper(:instance, event_attribute) do |machine, object|
+          # Interpret non-blank events as present
+          event = machine.read(object, :event, true)
+          event && !(event.respond_to?(:empty?) && event.empty?) ? event.to_sym : nil
+        end
+
+        # A roundabout way of writing the attribute is used here so that
+        # integrations can hook into this modification
+        define_helper(:instance, "#{event_attribute}=") do |machine, object, value|
+          machine.write(object, :event, value, true)
+        end
+
+        event_transition_attribute = attribute(:event_transition)
+        define_helper :instance, <<-END_EVAL, __FILE__, __LINE__ + 1
+              protected; attr_accessor #{event_transition_attribute.inspect}
+        END_EVAL
+      end
+
+      # Adds helper methods for getting information about this state machine's
+      # available transition paths
+      def define_path_helpers
+        # Gets the paths of transitions available to the current object
+        define_helper(:instance, attribute(:paths)) do |machine, object, *args|
+          machine.paths_for(object, *args)
+        end
+      end
+
+      # Adds helper methods for accessing naming information about states and
+      # events on the owner class
+      def define_name_helpers
+        # Gets the humanized version of a state
+        define_helper(:class, "human_#{attribute(:name)}") do |machine, klass, state|
+          machine.states.fetch(state).human_name(klass)
+        end
+
+        # Gets the humanized version of an event
+        define_helper(:class, "human_#{attribute(:event_name)}") do |machine, klass, event|
+          machine.events.fetch(event).human_name(klass)
+        end
+
+        # Gets the state name for the current value
+        define_helper(:instance, attribute(:name)) do |machine, object|
+          machine.states.match!(object).name
+        end
+
+        # Gets the human state name for the current value
+        define_helper(:instance, "human_#{attribute(:name)}") do |machine, object|
+          machine.states.match!(object).human_name(object.class)
+        end
+      end
+    end
+  end
+end
---
+++ lib/state_machines/machine/utilities.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module StateMachines
+  class Machine
+    module Utilities
+      protected
+
+      # Looks up other machines that have been defined in the owner class and
+      # are targeting the same attribute as this machine.  When accessing
+      # sibling machines, they will be automatically copied for the current
+      # class if they haven't been already.  This ensures that any configuration
+      # changes made to the sibling machines only affect this class and not any
+      # base class that may have originally defined the machine.
+      def sibling_machines
+        owner_class.state_machines.each_with_object([]) do |(name, machine), machines|
+          machines << (owner_class.state_machine(name) {}) if machine.attribute == attribute && machine != self
+        end
+      end
+
+      # Looks up the ancestor class that has the given method defined.  This
+      # is used to find the method owner which is used to determine where to
+      # define new methods.
+      def owner_class_ancestor_has_method?(scope, method)
+        return false unless owner_class_has_method?(scope, method)
+
+        superclasses = owner_class.ancestors.select { |ancestor| ancestor.is_a?(Class) }[1..]
+
+        if scope == :class
+          current = owner_class.singleton_class
+          superclass = superclasses.first
+        else
+          current = owner_class
+          superclass = owner_class.superclass
+        end
+
+        # Generate the list of modules that *only* occur in the owner class, but
+        # were included *prior* to the helper modules, in addition to the
+        # superclasses
+        ancestors = current.ancestors - superclass.ancestors + superclasses
+        ancestors = ancestors[ancestors.index(@helper_modules[scope])..].reverse
+
+        # Search for for the first ancestor that defined this method
+        ancestors.detect do |ancestor|
+          ancestor = ancestor.singleton_class if scope == :class && ancestor.is_a?(Class)
+          ancestor.method_defined?(method) || ancestor.private_method_defined?(method)
+        end
+      end
+
+      # Determines whether the given method is defined in the owner class or
+      # in a superclass.
+      def owner_class_has_method?(scope, method)
+        target = scope == :class ? owner_class.singleton_class : owner_class
+        target.method_defined?(method) || target.private_method_defined?(method)
+      end
+
+      # Pluralizes the given word using #pluralize (if available) or simply
+      # adding an "s" to the end of the word
+      def pluralize(word)
+        word = word.to_s
+        if word.respond_to?(:pluralize)
+          word.pluralize
+        else
+          "#{word}s"
+        end
+      end
+
+      # Generates the results for the given scope based on one or more states to
+      # filter by
+      def run_scope(scope, machine, klass, states)
+        values = states.flatten.compact.map { |state| machine.states.fetch(state).value }
+        scope.call(klass, values)
+      end
+
+      # Adds sibling machine configurations to the current machine.  This
+      # will add states from other machines that have the same attribute.
+      def add_sibling_machine_configs
+        # Add existing states
+        sibling_machines.each do |machine|
+          machine.states.each { |state| states << state unless states[state.name] }
+        end
+      end
+    end
+  end
+end
